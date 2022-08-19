130db41c2e61849da9556c5b649af7a39048e302
Tatiana Turin <tatiana.turin@gmail.com>
2022-08-19 13:57:53 -0400

    c

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class StaticObstacle(pygame.sprite.Sprite):
         self.image = pygame.Surface(size)
         self.image.fill('yellow')
         self.rect = self.image.get_rect(topleft=pos)
-        self.old_rect = self.rect.copy()
+        self.old_rect = self.rect.copy() #
 
 class MovingVerticalObstacle(StaticObstacle):
     def __init__(self, pos, size, groups):
@@ -70,7 +70,7 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.direction = pygame.math.Vector2()
         self.speed = 200
-        self.obstacles = obstacles
+        self.obstacles = obstacles #collision_sprites group is an attribute of the player, the player is not in the group
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -91,31 +91,33 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
 
     def collision(self, direction):
-        collision_sprites = pygame.sprite.spritecollide(self, self.obstacles, False)
+        collision_sprites = pygame.sprite.spritecollide(self, self.obstacles, False) #pygame method
+        print(collision_sprites)
         if collision_sprites:
             if direction == 'horizontal':
                 for sprite in collision_sprites:
                     # collision on the right
-                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left: #you need both conditionals to check. the first isn't suffice
                         self.rect.right = sprite.rect.left
                         self.pos.x = self.rect.x
-
-                    # collision on the left
-                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+        #
+        #             # collision on the left
+                    if self.rect.left<=sprite.rect.right and self.old_rect.left>=sprite.old_rect.right:
                         self.rect.left = sprite.rect.right
                         self.pos.x = self.rect.x
 
             if direction == 'vertical':
                 for sprite in collision_sprites:
-                    # collision on the bottom
+                    #collision on the top
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+                        self.pos.y = self.rect.y
+
+                    #collision on the bottom
                     if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
                         self.rect.bottom = sprite.rect.top
                         self.pos.y = self.rect.y
 
-                    # collision on the top
-                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
-                        self.rect.top = sprite.rect.bottom
-                        self.pos.y = self.rect.y
 
     def update(self, dt):
         self.old_rect = self.rect.copy()
@@ -203,7 +205,7 @@ class Ball(pygame.sprite.Sprite):
                 self.direction.y *= -1
 
     def update(self, dt):
-        self.old_rect = self.rect.copy()
+        self.old_rect = self.rect.copy() #every frame update you make a copy of the rectangle position and put it into old_rect
 
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
@@ -232,8 +234,9 @@ StaticObstacle((800,600),(100,200),[all_sprites,collision_sprites])
 StaticObstacle((900,200),(200,10),[all_sprites,collision_sprites])
 MovingVerticalObstacle((200,300),(200,60),[all_sprites,collision_sprites])
 MovingHorizontalObstacle((850,350),(100,100),[all_sprites,collision_sprites])
-# player = Player(all_sprites)
-# Ball(all_sprites)
+
+player = Player(all_sprites,collision_sprites)
+Ball(all_sprites,collision_sprites,player)
 
 #loop
 last_time = time.time()
@@ -244,6 +247,7 @@ while True:
     last_time = time.time()
 
     #event loop
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -251,7 +255,10 @@ while True:
 
     screen.fill('black')
     all_sprites.update(dt)
+    for sprite in all_sprites: #draw an organge rectangle underneath all of the sprites
+        pygame.draw.rect(screen, 'orange', sprite.old_rect)
     all_sprites.draw(screen)
+
 
     #display output
     pygame.display.update()
